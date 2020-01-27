9c088f895f952f3992e8e903804a9f8f87da9160
giuseppe <g.tutino1@studenti.uniba.it>
2020-01-27 13:50:18 +0100

    beta funzionante dell'upload dell'immagine

--- app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/ui/profile/ProfileFragment.java
+++ app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/ui/profile/ProfileFragment.java
@@ -1,11 +1,17 @@
 package it.uniba.di.sms1920.madminds.balanceout.ui.profile;
 
+import android.Manifest;
 import android.app.ActionBar;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,12 +20,15 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -30,6 +39,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
@@ -43,18 +54,28 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
 
 import it.uniba.di.sms1920.madminds.balanceout.MainActivity;
 import it.uniba.di.sms1920.madminds.balanceout.R;
 import it.uniba.di.sms1920.madminds.balanceout.helper.CircleTrasformation;
 import it.uniba.di.sms1920.madminds.balanceout.model.User;
+import it.uniba.di.sms1920.madminds.balanceout.ui.home.NewGroupActivity;
 import it.uniba.di.sms1920.madminds.balanceout.ui.settings.SettingsActivity;
+
+import static android.app.Activity.RESULT_OK;
 
 public class ProfileFragment extends Fragment {
 
     private static final int RC_SIGN_IN = 9001;
     public static final int LOGOUT_ID = 107;
+    public final int RESULT_LOAD_IMAGE=21;
+    public final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 11;
 
     private static final String TAG = "balanceOutTracker";
     private TextInputEditText emailEditText, passwordEditText;
@@ -71,8 +92,14 @@ public class ProfileFragment extends Fragment {
     private TextInputEditText emailProfileEditText;
     private MaterialButton modifyProfileMaterialButton;
     private MaterialButton saveModifyProfileMaterialButton;
+    private ImageView modifyprofileImageView;
+    private ImageView profileImagevView;
+
+    private Bitmap imgProfile = null;
 
     private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    private Uri filePath;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -103,6 +130,9 @@ public class ProfileFragment extends Fragment {
                 modifyProfileMaterialButton = root.findViewById(R.id.modifyPasswordMaterialButton);
                 saveModifyProfileMaterialButton = root.findViewById(R.id.saveModifyProfileMaterialButton);
 
+                modifyprofileImageView = root.findViewById(R.id.modifyeProfilemageView);
+                profileImagevView = root.findViewById(R.id.profileImageView);
+
                 nameProfileTextInputEditText.setFocusable(true);
                 surnameProfileEditText.setFocusable(true);
                 emailProfileEditText.setFocusable(true);
@@ -129,6 +159,9 @@ public class ProfileFragment extends Fragment {
 
         /* funzione che verifica se l'utente è loggato o meno e memorizza l'informazione in isLogged*/
         verifyLogged();
+
+
+        //TODO modificare la password solo se l'autenticazione è SENZA GOOGLE
 
 
         if(isLogged) {
@@ -247,15 +280,18 @@ public class ProfileFragment extends Fragment {
 
 
         saveModifyProfileMaterialButton = root.findViewById(R.id.saveModifyProfileMaterialButton);
+
         ActionBar actionBar = getActivity().getActionBar();
         final TextView emailTest, surnameTextView, nameTextView;
-        final ImageView profileImagevView;
+        //final ImageView profileImagevView;
         Button logout;
 
         emailTest = root.findViewById(R.id.emailProfileEditText);
         surnameTextView = root.findViewById(R.id.surnameProfileEditText);
         nameTextView = root.findViewById(R.id.nameProfileEditText);
+
         profileImagevView = root.findViewById(R.id.profileImageView);
+        modifyprofileImageView = root.findViewById(R.id.modifyeProfilemageView);
 
         /*
         emailTest.setText(firebaseUser.getEmail());
@@ -263,6 +299,7 @@ public class ProfileFragment extends Fragment {
          */
 
 
+        storageReference = FirebaseStorage.getInstance().getReference("images_first");
         databaseReference = FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.getUid());
 
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -286,7 +323,6 @@ public class ProfileFragment extends Fragment {
                 emailTest.setText(dataSnapshot.child("email").getValue().toString());
 
                 String filePath = dataSnapshot.child("picture").getValue().toString();
-                //profileImagevView.m
                 Picasso.get().load(filePath).fit().centerInside().transform(new CircleTrasformation()).into(profileImagevView);
 
 
@@ -300,12 +336,19 @@ public class ProfileFragment extends Fragment {
         });
 
 
+        modifyprofileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkPermissionReadExternalStorage();
+            }
+        });
 
 
         saveModifyProfileMaterialButton.setOnClickListener(new MaterialButton.OnClickListener(){
 
             @Override
             public void onClick(View v) {
+
                 if(nameProfileTextInputEditText.getText().toString().isEmpty() || surnameProfileEditText.getText().toString().isEmpty() ||
                     emailProfileEditText.getText().toString().isEmpty() ){
                     Toast.makeText(getActivity(), R.string.title_message_error_empty,
@@ -315,17 +358,114 @@ public class ProfileFragment extends Fragment {
                     saveModifyProfileMaterialButton.setVisibility(View.GONE);
                 }
 
+
+
             }
         });
 
+
+
         return root;
     }
+
+    private String getExtension(Uri uri){
+        ContentResolver cr= getActivity().getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return  mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+
+    }
+
+
+    private void fileUpdater(){
+
+        final StorageReference ref = storageReference.child(mAuth.getUid()+"."+getExtension(filePath));
+
+
+        ref.putFile(filePath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                        Toast.makeText(getActivity(),"Image Upload Succesfully",Toast.LENGTH_LONG).show();
+                        //Scrittura della posizione della foto nello storage
+
+
+                        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                            @Override
+                            public void onSuccess(Uri uri) {
+
+                                databaseReference.child("picture").setValue(uri.toString());
+                                Toast.makeText(getActivity(),"Save on DataBase",Toast.LENGTH_LONG).show();
+
+                            }
+                        });
+
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                    }
+                });
+
+
+    }
+
+
+    public void checkPermissionReadExternalStorage() {
+
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.READ_CONTACTS)) {
+                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+
+                // MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            /* Apro la galleria per selezionare la foto */
+            Intent i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, RESULT_LOAD_IMAGE);
+        }
+    }
+
+
+
 
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+
+        /*viene caricata l'immagine scelta dalla galleria nell image view*/
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+
+            filePath = data.getData();
+            profileImagevView.setPadding(9,9,9,9);
+            Picasso.get().load(filePath).fit().centerInside().transform(new CircleTrasformation()).into(profileImagevView);
+
+            fileUpdater();
+        }
+
+
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         switch (requestCode) {
