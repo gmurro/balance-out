7e605e5106ac96ae231dbbc5046be23e4574b461
0BugFound <alessio.tart@gmail.com>
2020-01-29 23:35:49 +0100

    added logic to "add expense"

--- app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/model/Expense.java
+++ app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/model/Expense.java
@@ -62,9 +62,6 @@ public class Expense {
         return payersExpense;
     }
 
-    public String getIdGroup() {
-        return idGroup;
-    }
 
     public String getData() {
         return data;
