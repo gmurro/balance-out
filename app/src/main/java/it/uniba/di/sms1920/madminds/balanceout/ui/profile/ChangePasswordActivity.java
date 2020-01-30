fce43ac0f49ed8737a4acb0c025a9fa8cf47ba7c
christiansoriani <c.soriani@studenti.uniba.it>
2020-01-30 18:47:57 +0100

    add change password

--- app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/ui/profile/ChangePasswordActivity.java
+++ app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/ui/profile/ChangePasswordActivity.java
@@ -1,16 +1,41 @@
 package it.uniba.di.sms1920.madminds.balanceout.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import it.uniba.di.sms1920.madminds.balanceout.MainActivity;
 import it.uniba.di.sms1920.madminds.balanceout.R;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.changePasswordToolbar);
+        setSupportActionBar(myToolbar);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_activity, R.id.navigation_notifications, R.id.navigation_profile)
+                .build();
+
+
     }
+
+
+
+
 }
