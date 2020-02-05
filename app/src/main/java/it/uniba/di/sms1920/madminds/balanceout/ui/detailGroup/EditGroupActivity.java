2ee94c868c2b2e24e22292d5a4afa23ba58863e4
gmurro <g.murro23@gmail.com>
2020-02-05 09:16:13 +0100

    edit group init

---
+++ app/src/main/java/it/uniba/di/sms1920/madminds/balanceout/ui/detailGroup/EditGroupActivity.java
@@ -0,0 +1,16 @@
+package it.uniba.di.sms1920.madminds.balanceout.ui.detailGroup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.uniba.di.sms1920.madminds.balanceout.R;
+
+public class EditGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_group);
+    }
+}
